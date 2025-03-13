dcdc2253ece8f385e5a9a57d1413704c808980a6
Shubh Arya <shubh.arya37@gmail.com>
2025-03-13 17:51:08 +0530

    added default styling

---
+++ tailwind.config.js
@@ -0,0 +1,91 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "*.{js,ts,jsx,tsx,mdx}",
+      "app/**/*.{ts,tsx}",
+      "components/**/*.{ts,tsx}",
+    ],
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {
+        colors: {
+          border: "hsl(var(--border))",
+          input: "hsl(var(--input))",
+          ring: "hsl(var(--ring))",
+          background: "hsl(var(--background))",
+          foreground: "hsl(var(--foreground))",
+          primary: {
+            DEFAULT: "hsl(var(--primary))",
+            foreground: "hsl(var(--primary-foreground))",
+          },
+          secondary: {
+            DEFAULT: "hsl(var(--secondary))",
+            foreground: "hsl(var(--secondary-foreground))",
+          },
+          destructive: {
+            DEFAULT: "hsl(var(--destructive))",
+            foreground: "hsl(var(--destructive-foreground))",
+          },
+          muted: {
+            DEFAULT: "hsl(var(--muted))",
+            foreground: "hsl(var(--muted-foreground))",
+          },
+          accent: {
+            DEFAULT: "hsl(var(--accent))",
+            foreground: "hsl(var(--accent-foreground))",
+          },
+          popover: {
+            DEFAULT: "hsl(var(--popover))",
+            foreground: "hsl(var(--popover-foreground))",
+          },
+          card: {
+            DEFAULT: "hsl(var(--card))",
+            foreground: "hsl(var(--card-foreground))",
+          },
+          coolblue: {
+            DEFAULT: "#002B5B",
+            50: "#E6EEF5",
+            100: "#CCDCEB",
+            200: "#99B9D7",
+            300: "#6696C3",
+            400: "#3373AF",
+            500: "#002B5B",
+            600: "#002249",
+            700: "#001A37",
+            800: "#001124",
+            900: "#000912",
+          },
+          coolyellow: {
+            DEFAULT: "#FFD100",
+            50: "#FFFAE6",
+            100: "#FFF5CC",
+            200: "#FFEB99",
+            300: "#FFE066",
+            400: "#FFD633",
+            500: "#FFD100",
+            600: "#CCA700",
+            700: "#997D00",
+            800: "#665400",
+            900: "#332A00",
+          },
+        },
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+      },
+    },
+    plugins: [require("tailwindcss-animate")],
+  }
+  
+  
