7b31b1bc40fb5c72c0a6abee56f4e9b0f4902a20
Shubh Arya <shubh.arya37@gmail.com>
2025-03-13 17:49:22 +0530

    added all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Landing from "./pages/Landing"
+import CaseStudiesPage from "./pages/CaseStudiesPage"
+import BeforeAfterPage from "./pages/BeforeAfterPage"
+import Projects from "./pages/Projects"
+import AboutPage from "./pages/AboutPage"
+import Layout from "./components/Layout"
+import CalculatorPage from "./pages/CalculatorPage"
 
 function App() {
 
   return (
     <>
-      
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="case-studies" element={<CaseStudiesPage />} />
+          <Route path="before-after" element={<BeforeAfterPage />} />
+          <Route path="calculator" element={<CalculatorPage />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+
     </>
   )
 }
