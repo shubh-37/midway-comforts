33dad6dc8fb828c764588413971d58dd4380e4a5
Shubh Arya <shubh.arya37@gmail.com>
2025-03-15 19:53:41 +0530

    added new sections

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection';
-import HVACServicePage from '@/components/Services';
+import HVACServicePage from '@/components/WhyChooseUsSec';
 import PremiumHVACSolutions from '@/components/Solutions';
+import BrandMarquee from '@/components/BrandsMarquee';
 
 export default function Landing() {
   return (
     <>
       <HeroSection />
+      <BrandMarquee />
       <PremiumHVACSolutions />
       <HVACServicePage />
     </>
