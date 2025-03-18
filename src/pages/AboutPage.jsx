4e5ddbab6814af0178119fd1238a4e09f26d2e6a
Shubh Arya <shubh.arya37@gmail.com>
2025-03-18 21:35:20 +0530

    Added the leadership section in the about us page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import AboutUsHeader from '@/components/AboutUsHeader';
 import CompanyTimeline from '@/components/CompanyJourney';
 import IndustriesSection from '@/components/Industries';
+import LeadershipCarousel from '@/components/Leadership';
 
 function AboutPage() {
   return (
     <div className="py-16 px-4 md:px-6">
       <AboutUsHeader />
       <CompanyTimeline />
+      <LeadershipCarousel />
       <IndustriesSection />
     </div>
   );
