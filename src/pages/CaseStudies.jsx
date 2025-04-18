9325f73e86f6d023084bbe0592c86f4c27d9008d
Shubh Arya <shubh.arya37@gmail.com>
2025-04-18 20:12:46 +0530

    renamed the page

---
+++ src/pages/CaseStudies.jsx
@@ -0,0 +1,11 @@
+import KeyProjects from '@/components/KeyProjects';
+
+const CaseStudies = () => {
+  return (
+    <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <KeyProjects />
+    </div>
+  );
+};
+
+export default CaseStudies;
